6fb93bf409f3f325a5d2c8b0b2fa4c709ce40148
AntonijaP <antonija.pofuk@gmail.com>
2021-02-18 13:41:09 +0100

    Fixed client form validation

--- AngularJsSample.Web/app/moviePersons/manage/manageMoviePerson.module.js
+++ AngularJsSample.Web/app/moviePersons/manage/manageMoviePerson.module.js
@@ -7,18 +7,18 @@
         .controller('manageMoviePersonCtrl', manageMoviePersonCtrl);
 
     manageMoviePersonCtrl.$inject = ['$scope', '$http', 'moviePerson', 'moviePersonsSvc', '$state'];
-    function manageMoviePersonCtrl($scope, $http, moviePerson, moviePersonsSvc, $state ) {
-        
+    function manageMoviePersonCtrl($scope, $http, moviePerson, moviePersonsSvc, $state) {
+
         //#region JS variables
-        var vm = this;   
-      
+        var vm = this;
+
         //#endregion 
 
         //#region Bindable Members
         vm.moviePerson = moviePerson.data; // data is in general.routing.js
         vm.save = saveMoviePerson;
-        vm.datepicker = datePicker;      
-       
+        vm.datepicker = datePicker;
+
         //#endregion
 
         //#region On activate
@@ -26,27 +26,31 @@
         //#endregion
 
         //#region JS functions
-        function activate() {}
+        function activate() { }
 
 
         function saveMoviePerson(dataItem) {
-            if (vm.moviePerson.id>0) {
+            if (vm.moviePerson.id === undefined) {
+                //create               
+                    moviePersonsSvc.createMoviePerson(vm.moviePerson).then(function () {
+                        Swal.fire('Uspješno ste kreirali novu osobu!')
+                        $state.go("moviePersonsOverview");
+                    }, function (error) {
+                            Swal.fire(error.status + ': Niste uspješno dodali osobu!')
+                    });                 
+            } else {
                 //update
                 moviePersonsSvc.updateMoviePerson(dataItem.id, vm.moviePerson).then(function () {
+                    Swal.fire('Uspješno ste ažurirali osobu!')
                     $state.go("moviePersonsOverview");
                 }, function (error) {
+                    Swal.fire(error.status + ': Niste uspješno ažurirali osobu!')
                     //add error handling
                 });
+
             }
-            else {
-                //create
-                moviePersonsSvc.createMoviePerson(vm.moviePerson).then(function () {
-                    $state.go("moviePersonsOverview");
-                }, function (error) {
-                    //add error handling
-                });
-            }
-        }             
+        }
+
 
         function datePicker() {
             monthPickerConfig = {
@@ -57,15 +61,15 @@
 
         $scope.dialog = {
             message: ""
-        }        
+        }
         $scope.showDialog = function (title, message) {
             $scope.dialog.message = message;
             $scope.deleteDialogWindow.title(title);
             $scope.deleteDialogWindow.center();
             $scope.deleteDialogWindow.open();
-        } 
+        }
 
-    }      
-        //#endregion
+    }
+    //#endregion
 
 })();
