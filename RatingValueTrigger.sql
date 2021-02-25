48e2f8ca1f8e45b022d62944e2927de20884f7e5
AntonijaP <antonija.pofuk@gmail.com>
2021-02-25 11:49:04 +0100

    Fixed rating control trigger

--- RatingValueTrigger.sql
+++ RatingValueTrigger.sql
@@ -1,29 +1,15 @@
---====================================
---  Create database trigger template 
---====================================
-USE AngularJsSample
-GO
+  CREATE  OR ALTER TRIGGER RatingControl2    
+  ON MovieRating 
+  AFTER INSERT, UPDATE 
+  AS 
+  BEGIN  
+    UPDATE Movie SET Rating = (
+      Select AVG(Rating) 
+      from MovieRating
+	  	  WHERE MovieId = (SELECT Id FROM inserted)
+	  GROUP BY MovieId
+      )
+	WHERE Id = (SELECT Id FROM inserted)
 
-CREATE OR ALTER TRIGGER RatingControl  
-ON Movie
-AFTER INSERT, UPDATE AS BEGIN
-
-UPDATE Movie 
-SET 
-Movie.Rating = (SELECT(SUM(i.Rating+MovieRating.Rating))/2
-FROM MovieRating 
-INNER JOIN inserted i 
-ON MovieRating.MovieId = i.Id
-GROUP BY MovieRating.MovieId
-)
-FROM inserted i
-WHERE Movie.Id = i.Id
-
-
-END
-GO
-
-
-
-      
+	END
 
